af87840cd4363409d7f86eb21c081a47c9465a7d
Bloodyly <155741658+Bloodyly@users.noreply.github.com>
2026-01-09 18:15:55 +0100

    Revert "Add 10x10 status icons; improve read UI, UID formatting, tag-type handling and HID gating"

--- MainApplication/images/FlipUIDConv_icons.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <gui/icon.h>
-
-extern const Icon I_FlipUIDConv_Sound_10x10;
-extern const Icon I_FlipUIDConv_Mute_10x10;
-extern const Icon I_FlipUIDConv_UsbOn_10x10;
-extern const Icon I_FlipUIDConv_UsbOff_10x10;
